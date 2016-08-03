32461057a3fcefae0efd9a259f5da3ff190b44c3
Raoul Scarazzini <rscarazz@redhat.com>
2016-08-03 11:29:38 -0400

    Adding suggestions for rhos-release repo configuration

--- scripts/undercloud-repos-osp7.sh
+++ scripts/undercloud-repos-osp7.sh
@@ -4,4 +4,7 @@ echo $(date)
 
 yum install -y http://rhos-release.virt.bos.redhat.com/repos/rhos-release/rhos-release-latest.noarch.rpm
 rhos-release 7-director
+# To pick a specific release
+#rhos-release -p Y3.1 7-director -r 7.2
+#rhos-release -p Z4 7
 yum install -y python-rdomanager-oscplugin
