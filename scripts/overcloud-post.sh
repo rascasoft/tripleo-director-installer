ba1bac4943c157ad8851dede2f4d6004cf9e4e84
Raoul Scarazzini <rscarazz@redhat.com>
2016-10-07 05:16:11 -0400

    Ensuring sos package gets installed on the overcloud

--- scripts/overcloud-post.sh
+++ scripts/overcloud-post.sh
@@ -22,13 +22,29 @@ chmod 600 /home/stack/.ssh/config
 
 echo "$(date) Generating sosreports"
 
+# Getting sos package
+sudo yum reinstall --downloadonly  --downloaddir=. sos
+
 mkdir -p /tmp/sosreports
 for i in $(nova list | grep overcloud  | awk '{ print $12; }' | cut -f2 -d=)
 do
- $SSH $i "sudo sosreport --batch; sudo mv -v /var/tmp/sosre*xz /tmp; sudo chmod 644 /tmp/sosrep*xz"
+ $SCP ./sos*.rpm $i:
+ $SSH $i "sudo rpm -Uvh --force sos*.rpm; sudo sosreport --batch; sudo mv -v /var/tmp/sosre*xz /tmp; sudo chmod 644 /tmp/sosrep*xz"
  $SCP $i:/tmp/sosrep*xz /tmp/sosreports/
 done
 
 sudo sosreport --batch || true
 sudo mv /var/tmp/sosre*xz /tmp/sosreports || true
 sudo chmod 644 /tmp/sosreports/*
+
+## WORKAROUND per https://bugzilla.redhat.com/show_bug.cgi?id=1349493
+#wget http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/server/7/7Server/x86_64/highavailability/os/Packages/pacemaker-1.1.13-10.el7_2.4.x86_64.rpm
+#wget http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/server/7/7Server/x86_64/highavailability/os/Packages/pacemaker-cli-1.1.13-10.el7_2.4.x86_64.rpm
+#wget http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/server/7/7Server/x86_64/highavailability/os/Packages/pacemaker-cluster-libs-1.1.13-10.el7_2.4.x86_64.rpm
+#wget http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/server/7/7Server/x86_64/highavailability/os/Packages/pacemaker-libs-1.1.13-10.el7_2.4.x86_64.rpm
+#wget http://pulp-read.dist.prod.ext.phx2.redhat.com/content/dist/rhel/server/7/7Server/x86_64/highavailability/os/Packages/pacemaker-remote-1.1.13-10.el7_2.4.x86_64.rpm
+#for i in $(nova list | grep overcloud  | awk '{ print $12; }' | cut -f2 -d=)
+#do
+# $SCP pacemaker-* $i:
+# $SSH $i "sudo rpm -Uvh pacemaker*"
+#done
