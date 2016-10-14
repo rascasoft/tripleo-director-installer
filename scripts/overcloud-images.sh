d485e35e4167b342a0f23dfef73b968baeede568
Raoul Scarazzini <rscarazz@redhat.com>
2016-10-14 09:37:24 -0400

    Fixing url for updated osp10 images

--- scripts/overcloud-images.sh
+++ scripts/overcloud-images.sh
@@ -9,8 +9,9 @@ source stackrc
 case $OPENSTACK_VERSION in
 "mitaka") IMAGE_URL="https://ci.centos.org/artifacts/rdo/images/mitaka/delorean/stable/"
           ;;
-"osp10") IMAGE_URL="http://rhos-release.virt.bos.redhat.com/ci-images/rhos-10/current-passed-ci/"
-        ;;
+"osp10") #IMAGE_URL="http://rhos-release.virt.bos.redhat.com/ci-images/rhos-10/current-passed-ci/"
+         IMAGE_URL="http://rhos-release.virt.bos.redhat.com/ci-images/rhos-10/2016-10-07.4/"
+         ;;
 "osp9") IMAGE_URL="http://rhos-release.virt.bos.redhat.com/ci-images/rhos-9/current-passed-ci/"
         ;;
 "osp8") IMAGE_URL="http://rhos-release.virt.bos.redhat.com/ci-images/rhos-8/current-passed-ci/"
@@ -49,6 +50,26 @@ case $OPENSTACK_VERSION in
         sudo -E bash -c "virt-customize --verbose -a overcloud-full.qcow2 --selinux-relabel --memsize 8192 --smp 4 --root-password password:redhat --run-command 'yum localinstall -y http://rhos-release.virt.bos.redhat.com/repos/rhos-release/rhos-release-latest.noarch.rpm http://rhos-release.virt.bos.redhat.com/repos/rhos-release/extras/7/wget-1.14-9.el7.x86_64.rpm http://rhos-release.virt.bos.redhat.com/repos/rhos-release/extras/7/yum-utils-1.1.31-29.el7.noarch.rpm' --run-command 'rhos-release -p latest 7' --run-command 'yum -y -v update > /tmp/yum-update.log 2>&1'" || /bin/true
         ;;
 esac
+
+# This patch can be used in osp7 to solve this bug https://bugzilla.redhat.com/show_bug.cgi?id=1384068
+#*** /usr/share/openstack-tripleo-heat-templates/extraconfig/tasks/pacemaker_resource_restart.sh.org     2016-10-12 12:33:06.375152558 -0400
+#--- /usr/share/openstack-tripleo-heat-templates/extraconfig/tasks/pacemaker_resource_restart.sh 2016-10-12 12:24:26.059431200 -0400
+#***************
+#*** 36,41 ****
+#--- 36,42 ----
+#  }
+#
+#  if [ "$pacemaker_status" = "active" -a \
+#+      "$(hiera update_indentifier)" != "nil" -a \
+#       "$(hiera bootstrap_nodeid)" = "$(facter hostname)" ]; then
+#
+#      #ensure neutron constraints like
+#
+#        sed -i -e 's/if [ "$pacemaker_status" = "active" -a 
+#     "$(hiera update_indentifier)" != nil \
+#     "$(hiera bootstrap_nodeid)" = "$(facter hostname)" ]; then' /usr/share/openstack-tripleo-heat-templates/extraconfig/tasks/pacemaker_resource_restart.sh
+#        ;;
+#esac
 
 # To generate mitaka images:
 #export NODE_DIST=centos7
